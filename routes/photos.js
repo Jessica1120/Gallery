26eb56b3736ab52a28a466a433d6a1b9bd4f2326
Jessica Brown <jessicakbrown1120@gmail.com>
2017-09-25 09:13:49 -0500

    photos displaying

--- routes/photos.js
+++ routes/photos.js
@@ -1,5 +1,5 @@
 var router = require('express').Router();
-var photos = [{image: '../public/images/Boodle.jpg'}];
+var photos = [{image: './images/Boodle.jpg'}, {image: './images/Ladyfingers.jpg'}, {image: './images/Flag.jpg'}, {image: './images/Dogs.jpg'}];
 
 router.get('/', function(req, res) {
     console.log('get photos');
