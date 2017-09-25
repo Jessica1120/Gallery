2d95c822b8a6ae9951afd697e52c05cf56dc5ae0
Jessica Brown <jessicakbrown1120@gmail.com>
2017-09-25 12:36:05 -0500

    Toggle works

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,9 +13,25 @@ myApp.controller('PhotoController', function($http) {
             method: 'GET',
             url:    '/photos'
         }).then(function(res){
-            vm.photos = res.data;
+            vm.photos = res.data; //res is whole response, data is array
             console.log('vm.photos:', vm.photos);
         }); // end then
     }; //end Get
     vm.getPhotos();
+
+    vm.toggle = function (photo) {
+        photo.see = !photo.see;
+        console.log(photo.see);
+    };
+    vm.postYes = function(req, req) {
+        $http({
+            method: ''
+        })
+    }
 });// end PC
+
+
+//dev's Toggle   
+//vm.toggleMe = function(index) {
+  //          console.log('toggle?', index)
+    //        vm.photos[index].show = 
--- routes/photos.js
+++ routes/photos.js
@@ -1,5 +1,5 @@
 var router = require('express').Router();
-var photos = [{image: './images/Boodle.jpg'}, {image: './images/Ladyfingers.jpg'}, {image: './images/Flag.jpg'}, {image: './images/Dogs.jpg'}];
+var photos = [{image: './images/Boodle.jpg', caption: 'Gruffles'}, {image: './images/Ladyfingers.jpg', caption: '#teamladyref in NOLA'}, {image: './images/Flag.jpg', caption: 'Art by Dad'}, {image: './images/Dogs.jpg', caption: 'Mollie and Marla'}];
 
 router.get('/', function(req, res) {
     console.log('get photos');
